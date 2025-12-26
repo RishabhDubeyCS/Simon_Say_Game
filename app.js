b8f1afab499c6dcdcf756e578e947d87a8aae472
rishabh dubey <rishabhdubeycse>
2025-12-26 18:25:56 +0530

    Inital commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Event Bubbling Example  parent element is also printing when child is clicked to Solve this issue we can use event.stopPropagation() method
+
+// Event Bubbling Example
+
+let div = document.querySelector("div");
+let ul = document.querySelector("ul");
+let lis = document.querySelectorAll("li");
+
+div.addEventListener("click", function() {
+   
+    console.log("div clicked");
+});
+
+ul.addEventListener("click", function(event) {
+    event.stopPropagation();
+    console.log("ul clicked");
+});
+
+
+for (let li of lis) {
+    li.addEventListener("click", function(event) {
+        event.stopPropagation();
+        console.log("li clicked");
+    });
+}
+
+// Stop parent click events if needed
+        // event.stopPropagation();
